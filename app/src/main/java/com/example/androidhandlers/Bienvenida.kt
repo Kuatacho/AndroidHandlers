973e5867260ce671433ae9289e75b98e9a46649e
Victor Jesus Chavez Alvarez <cav2024930@est.univalle.edu>
2022-09-22 08:53:46 -0400

    Pasado de pantalla

--- app/src/main/java/com/example/androidhandlers/Bienvenida.kt
+++ app/src/main/java/com/example/androidhandlers/Bienvenida.kt
@@ -2,10 +2,22 @@ package com.example.androidhandlers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.androidhandlers.databinding.ActivityBienvenidaBinding
 
 class Bienvenida : AppCompatActivity() {
+    private lateinit var binding:ActivityBienvenidaBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bienvenida)
+        binding=ActivityBienvenidaBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        //si esta pantalla ha sido lanzado producto de una Intencion en un objeto llammado intent
+        //queda registro para ser utilizado
+        val mensaje="""
+            El texto es: ${intent.getStringExtra("texto")}
+            El numero es: ${intent.getIntExtra("valor",0)}
+        """.trimIndent()
+        binding.txtResultado.text=mensaje
     }
 }
--- app/src/main/java/com/example/androidhandlers/MainActivity.kt
+++ app/src/main/java/com/example/androidhandlers/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.example.androidhandlers
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.androidhandlers.databinding.ActivityMainBinding
+
 /*Gradle scripts/buildgraddle(Module)-> Activamos el view binding VERIFICAR!*/
 //Creamos nueva actividad en java -> New Activity Empty
 class MainActivity : AppCompatActivity() {
+    //escoger correspondiente activity correspondiente a la pantalla
+    private lateinit var binding:ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnEjecutar.setOnClickListener{pasarPantalla()}
+    }
+
+    private fun pasarPantalla() {
+        val mensaje="Texto de Ejemplo"
+        val numero =10
+        //Intent para lanzar a una pantalla
+        //Paso de datos a traves de algo denominado
+        val intent=Intent(this,Bienvenida::class.java)
+        intent.apply {
+            putExtra("texto",mensaje)
+            putExtra("valor",numero)
+        }
+        startActivity(intent)
+
     }
 }
